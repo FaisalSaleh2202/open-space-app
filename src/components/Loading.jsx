7017e3f810beb45ae4989cc3fb7c5126c99c5b2d
FaisalSaleh2202 <faisal.saleh127@gmail.com>
2023-06-10 07:00:31 +0700

    initial commit

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import LoadingBar from 'react-redux-loading-bar'
+import LoadingBar from 'react-redux-loading-bar';
+
+function Loading() {
+  return (
+    <div className="loading">
+      {/* @TODO: use react-redux-loading-bar to show loading bar */}
+      <LoadingBar />
+    </div>
+  );
+}
+
+export default Loading;
